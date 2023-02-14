c540471d5fb2adec006821e5faf284bb93850716
Ömer Faruk YILMAZ <109068669+ArPharafin@users.noreply.github.com>
2023-02-14 17:57:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string konsolNerde = "nerde la";
+int ogrenmegunSayisi = 24;
+double videoDakika = 3.25;
+bool videoIzlemismi = false;
+if (videoIzlemismi == true)
+{
+    Console.WriteLine("Oha nasil izledin la");
+}
+else
+{
+    Console.WriteLine("Nasil izlemezsin senin ben varya");
+}
+for (int i = 0; i <10; i++) 
+{
+    Console.WriteLine("oha fora bak \n");
+
+}
