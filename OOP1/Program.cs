f0d00468f728dd6a90ebcd395671db1024cda0ff
Ömer Faruk YILMAZ <109068669+ArPharafin@users.noreply.github.com>
2023-02-17 18:43:41 +0300

    17 Subat Ders Sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1= new Product();
+            product1.Id= 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Lamba";
+            product1.UnitPrice = 325;
+            product1.UnitsInStock = 2;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5,
+                UnitsInStock = 5,ProductName="Kartus",UnitPrice=23};
+            //pascal case  //Camel Case
+            ProductManager productManager= new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+            //iint double,bool... deger tip
+            //diziler,class,abstracts class,interface... referans tip
+       
+        }
+
+        }
+    }
+
